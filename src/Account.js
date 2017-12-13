d59833c6786144e1de351f204b030246702f9003
kmlm <kevin.mcaleese@libertymutual.com>
2017-12-13 15:57:18 -0500

    Initial Respone with separate inputs

--- src/Account.js
+++ src/Account.js
@@ -9,23 +9,46 @@ export default class Account extends Component {
     }
 
     this.handleDepositClick = this.handleDepositClick.bind(this)
+      this.handleWithdrawalClick = this.handleWithdrawalClick.bind(this)
   }
 
   handleDepositClick(e) {
     e.preventDefault();
-    if (isNaN(this.refs.amount.value)) {
-      console.log("Not a number");
+    if (isNaN(this.refs.depositAmount.value) || this.refs.depositAmount.value < 0) {
+      console.log("Not a am acceptable  number");
     }
     else {
-      let amount = +this.refs.amount.value;
-      let newBalance = this.state.balance + amount;
+      let depositAmount = +this.refs.depositAmount.value;
+      let newBalance = this.state.balance + depositAmount;
       this.setState({
         balance: newBalance
       })
-      this.refs.amount.value = '';
+      this.refs.depositAmount.value = '';
     }
   }
 
+    handleWithdrawalClick(e) {
+        e.preventDefault();
+        if (isNaN(this.refs.withdrawalAmount.value)) {
+            console.log("Not a number");
+        }
+        else {
+            let withdrawalAmount = +this.refs.withdrawalAmount.value;
+            if (this.state.balance < withdrawalAmount || this.state.balance === withdrawalAmount) {
+                this.setState({
+                    balance: 0
+                })
+                this.refs.withdrawalAmount.value = '';
+            }
+            else {
+                let newBalance = this.state.balance - withdrawalAmount;
+                this.setState({
+                    balance: newBalance
+                })
+                this.refs.withdrawalAmount.value = '';
+            }
+        }
+        }
   render() {
     let balanceClass = 'balance';
     if (this.state.balance === 0) {
@@ -36,8 +59,11 @@ export default class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className={balanceClass}>${this.state.balance}</div>
-        <input type="text" placeholder="enter an amount" ref="amount" />
+        <input type="text" placeholder="enter an amount" ref="depositAmount" />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+
+          <input type="text" placeholder="enter an amount" ref="withdrawalAmount" />
+          <input type="button" value="Withdrawal" onClick={this.handleWithdrawalClick} />
       </div>
     )
   }
